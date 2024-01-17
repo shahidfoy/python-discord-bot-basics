f4d664a76dbb3df6e0576f784105a740be0bef6e
Shahid Foy <shahidfoy@gmail.com>
2024-01-17 16:50:03 -0700

    event-basics

--- BotManager.py
+++ BotManager.py
@@ -14,4 +14,21 @@ bot_client = discord.Client(intents=intents)
 async def on_ready():
     print("Bot is online..")
 
+@bot.event
+async def on_message(msg):
+    username = msg.author.display_name
+    if msg.author == bot.user:
+        return
+    else:
+        if msg.content == "hello python bot":
+            await msg.channel.send("hello " + username)
+
+@bot.event
+async def on_member_join(member):
+    print("New member is joining")
+    guild = member.guild
+    guildname = guild.name
+    dmchannel = await member.create_dm()
+    await dmchannel.send(f"Welcome to {guildname}!")
+
 bot.run("<BOT-SDK-KEY>")
